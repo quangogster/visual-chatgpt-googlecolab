06769897d07de7c16844260212cb585586bcc328
Quang <q@thrifa.io>
2023-03-11 16:22:07 -0800

    add missing com

--- visual_chatgpt.py
+++ visual_chatgpt.py
@@ -829,7 +829,7 @@ class ConversationBot:
                              "The input to this tool should be a string, representing the image_path. "),
             Tool(name="Generate Image From User Input Text", func=self.t2i.inference,
                  description="useful when you want to generate an image from a user input text and save it to a file. like: generate an image of an object or something, or generate an image that includes some objects. "
-                             "The input to this tool should be a string, representing the text used to generate image. ")
+                             "The input to this tool should be a string, representing the text used to generate image. "),
             Tool(name="Remove Something From The Photo", func=self.edit.remove_part_of_image,
                  description="useful when you want to remove and object or something from the photo from its description or location. "
                              "The input to this tool should be a comma seperated string of two, representing the image_path and the object need to be removed. ")]
